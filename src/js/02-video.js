45db5f602cdd150090a3b523605e47e37fa1e9e2
yovbaks <yovbak.s@gmail.com>
2022-05-10 21:41:04 +0300

    all

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+let time = localStorage.getItem('videoplayer-current-time');
+if (time != null) {
+  player.setCurrentTime(time);
+}
+
+const onCurrentTime = () => {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
+};
+
+player.on('timeupdate', throttle(onCurrentTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('.feedback-form input'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+let feedbackFormData = {};
+populateStorage();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (refs.form.email.value && refs.form.message.value) {
+    console.log(feedbackFormData);
+    feedbackFormData = {};
+    event.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
+
+function onFormInput(event) {
+  const name = event.target.name;
+  const message = event.target.value;
+
+  if (feedbackFormData) {
+    feedbackFormData[name] = message;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));
+  }
+}
+
+function populateStorage() {
+    
+  if (localStorage.getItem(STORAGE_KEY)) {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    feedbackFormData = JSON.parse(savedData);
+  }
+  if (feedbackFormData.email) {
+    refs.input.value = feedbackFormData.email;
+  }
+  if (feedbackFormData.message) {
+    refs.textarea.value = feedbackFormData.message;
+  }
+}
